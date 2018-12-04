97268dd569a0b0577cbd1dce6a3c8a56f7deea12
Alfonso Peterssen <a2peterssen@gmail.com>
2018-12-04 01:42:34 +0100

    Start from 0.

--- demo/santa.js
+++ demo/santa.js
@@ -1,1 +1,14 @@
 jQuery("#main-content > h1, #main-content > div.right-col > div:nth-child(1) > div.presentation > h4").text("Alfonso² Peterssen"); 
+
+var cssId = 'santa';
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://mukel.github.io/demo/santa.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
